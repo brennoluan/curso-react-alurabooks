c586453008bc032b61aa72649a21ab5e4dc3be5d
brenno luan <brennoluan8530@gmail.com>
2026-06-18 18:52:35 -0300

    Add loading state and Loader component to Categoria for improved user experience during data fetching

--- src/paginas/Categoria/index.tsx
+++ src/paginas/Categoria/index.tsx
@@ -3,12 +3,16 @@ import TituloPrincipal from '../../componentes/TituloPrincipal';
 import { ICategoria } from '../../interfaces/ICategoria';
 import http from '../../http';
 import { useParams } from 'react-router-dom';
+import Loader from '../../componentes/Loader';
 
 const Categoria = () => {
   const [categoria, setCategoria] = useState<ICategoria>();
+  const [loading, setLoading] = useState(true);
+
   const params = useParams();
 
   useEffect(() => {
+    setLoading(true);
     http
       .get<ICategoria[]>('/categorias', {
         params: {
@@ -17,8 +21,13 @@ const Categoria = () => {
       })
       .then((response) => {
         setCategoria(response.data[0]);
+        setLoading(false);
       });
   }, [params.slug]);
+
+  if (loading) {
+    return <Loader />;
+  }
 
   return (
     <section>
