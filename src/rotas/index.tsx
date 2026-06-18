2f71d0dad3ca3a52325d6a0115cb44304612883d
brenno luan <brennoluan8530@gmail.com>
2026-06-18 18:43:15 -0300

    Add route for Categoria component in Rotas

--- src/rotas/index.tsx
+++ src/rotas/index.tsx
@@ -1,19 +1,22 @@
-import { Route, Routes } from "react-router-dom"
-import AreaLogada from "../paginas/AreaLogada"
-import Home from "../paginas/Home"
-import PaginaBase from "../paginas/PaginaBase"
-import Pedidos from "../paginas/Pedidos"
-
+import { Route, Routes } from 'react-router-dom';
+import AreaLogada from '../paginas/AreaLogada';
+import Home from '../paginas/Home';
+import PaginaBase from '../paginas/PaginaBase';
+import Pedidos from '../paginas/Pedidos';
+import Categoria from '../paginas/Categoria';
 
 const Rotas = () => {
-    return (<Routes>
-      <Route path='/' element={<PaginaBase />}>
-        <Route path='/' element={<Home />} />
-        <Route path='/minha-conta' element={<AreaLogada />}>
-            <Route path="pedidos" element={<Pedidos />} />
-         </Route>
+  return (
+    <Routes>
+      <Route path="/" element={<PaginaBase />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/minha-conta" element={<AreaLogada />}>
+          <Route path="pedidos" element={<Pedidos />} />
+        </Route>
+        <Route path="/categorias/:slug" element={<Categoria />} />
       </Route>
-    </Routes>)
-}
+    </Routes>
+  );
+};
 
-export default Rotas
+export default Rotas;
