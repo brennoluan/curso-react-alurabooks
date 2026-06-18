d37b2f9cb4430bb230687209f6dd95425f0828d7
brenno luan <brennoluan8530@gmail.com>
2026-06-18 19:05:06 -0300

    Refactor HTTP client setup and add category fetching function for improved API interaction

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,24 +1,38 @@
-import axios from "axios";
+import axios from 'axios';
+import { ICategoria } from '../interfaces/ICategoria';
 
 const http = axios.create({
-    baseURL: 'http://localhost:8000',
-    headers: {
-        Accept: 'application/json',
-        Content: 'application/json'
-    }
-})
+  baseURL: 'http://localhost:8000',
+  headers: {
+    Accept: 'application/json',
+    Content: 'application/json',
+  },
+});
 
-http.interceptors.request.use(function (config) {
+http.interceptors.request.use(
+  function (config) {
     // Do something before request is sent
-    const token = sessionStorage.getItem('token')
+    const token = sessionStorage.getItem('token');
     if (token && config.headers) {
-        config.headers.Authorization = `Bearer ${token}`
+      config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
-  }, function (error) {
+  },
+  function (error) {
     // Do something with request error
-    console.log('Erro no interceptor do axios')
+    console.log('Erro no interceptor do axios');
     return Promise.reject(error);
+  },
+);
+
+export default http;
+
+export const obterCategoriaPorSlug = async (slug: string) => {
+  const response = await http.get<ICategoria[]>('/categorias', {
+    params: {
+      slug,
+    },
   });
 
-export default http
+  return response.data[0];
+};
