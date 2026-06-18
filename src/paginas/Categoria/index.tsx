0e906a86bf8740008890b24735d79f9eb1155882
brenno luan <brennoluan8530@gmail.com>
2026-06-18 18:46:26 -0300

    Implement category fetching in Categoria component and update title display

--- src/paginas/Categoria/index.tsx
+++ src/paginas/Categoria/index.tsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from 'react';
 import TituloPrincipal from '../../componentes/TituloPrincipal';
+import { ICategoria } from '../../interfaces/ICategoria';
+import http from '../../http';
+import { useParams } from 'react-router-dom';
 
 const Categoria = () => {
+  const [categoria, setCategoria] = useState<ICategoria>();
+  const params = useParams();
+
+  useEffect(() => {
+    http
+      .get<ICategoria[]>('/categorias', {
+        params: {
+          slug: params.slug,
+        },
+      })
+      .then((response) => {
+        setCategoria(response.data[0]);
+      });
+  }, [params.slug]);
+
   return (
     <section>
-      <TituloPrincipal texto="Categoria" />
+      <TituloPrincipal texto={categoria?.nome ?? ''} />
     </section>
   );
 };
