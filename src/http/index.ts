c696a67480990c574f96941e4b119fc5fd90e902
brenno luan <brennoluan8530@gmail.com>
2026-06-18 19:27:58 -0300

    Add function to fetch books by category from the API, enhancing data retrieval for categorized book listings.

--- src/http/index.ts
+++ src/http/index.ts
@@ -42,3 +42,13 @@ export const obterLivrosDestaque = async (tipo: string) => {
   const resposta = await http.get<ILivro[]>(`public/${tipo}`);
   return resposta.data;
 };
+
+export const obterCategoriaPorCategoria = async (categoria: ICategoria) => {
+  const response = await http.get<ILivro[]>('/livros', {
+    params: {
+      categoria: categoria.id,
+    },
+  });
+
+  return response.data;
+};
